d41c162225f7698bd03f710405bb92f605395f10
SavvasZoeCen <74330326+SavvasZoeCen@users.noreply.github.com>
2021-07-25 22:23:43 -0700

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -1,21 +1,28 @@
 from zksk import Secret, DLRep
 from zksk import utils
 
-def ZK_equality(G,H):
-    print("start the code")
-    #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
+
+def ZK_equality(G, H):
+
+    m = Secret(utils.get_random_num(bits=128))
     r1 = Secret(utils.get_random_num(bits=128))
     r2 = Secret(utils.get_random_num(bits=128))
-    m = 1
-    C1 = r1.value * G
-    C2 = r1.value * H + m * G
-    D1 = r2.value * G
-    D2 = r2.value * H + m * G
 
-    #Generate a NIZK proving equality of the plaintexts
-    stmt = DLRep(C1,r1*G) | DLRep(C2,r1*H+m*G) | DLRep(D1,r2*G) | DLRep(D2,r2*H+m*G)
+    # Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
+    C1, C2=generateCiphertexts(r1, m, G, H)
+    D1, D2=generateCiphertexts(r2, m, G, H)
+
+    # Generate a NIZK proving equality of the plaintexts
+    stmt = DLRep(C1, r1*G) & DLRep(C2, r1*H+m *
+               G) & DLRep(D1, r2*G) & DLRep(D2, r2*H+m*G)
     zk_proof = stmt.prove()
+    
+    # Return two ciphertexts and the proof
+    return (C1, C2), (D1, D2), zk_proof
 
-    #Return two ciphertexts and the proof
-    return (C1,C2), (D1,D2), zk_proof
 
+# Helper funtion
+def generateCiphertexts(r, m, G, H):
+    x = r.value * G
+    y = r.value * H + m.value * G
+    return x, y
